b5a4be06ec3df1637432d836bbf17a17de06790b
Paul Woods <itekhi@yandex.ru>
2022-11-26 05:34:38 +0100

    fixed some image paths

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 
+# first commit to main branch
+git add .
+git commit -am "$*"
+
+# build site
 gulp build
 
 cd dist
 
+# then commit to the second branch
 git add .
 git commit -am "$*"
 git push origin gh-pages
